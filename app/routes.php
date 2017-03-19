7086a562432116f8af87f8c1b5bcd2cf8712cb9e
Chris Emerson <chris@cemerson.co.uk>
2017-03-19 06:31:40 +0000

    aorenuda£

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,11 @@
 
 use BigGit\BigGit\Actions\OwnCloudFilesChangedAction;
 use BigGit\BigGit\Actions\HomeAction;
+use BigGit\BigGit\Actions\ResolveConflictsAction;
+
+$app->get('/', HomeAction::class);
 
 $app->post('/fileschanged', OwnCloudFilesChangedAction::class);
 $app->get('/fileschanged', OwnCloudFilesChangedAction::class);
+
+$app->get('/resolve/{hash1:[a-f0-9]{32}}..{hash2:[a-f0-9]{32}}', ResolveConflictsAction::class);
--- src/Actions/ResolveConflictsAction.php
+++ src/Actions/ResolveConflictsAction.php
@@ -4,10 +4,10 @@ namespace BigGit\BigGit\Actions;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-class HomeAction extends AbstractAction
+class ResolveConflictsAction extends AbstractAction
 {
     function __invoke(Request $request, Response $response, array $args)
     {
-        return $this->render($response, 'index.twig', $args);
+
     }
 }
