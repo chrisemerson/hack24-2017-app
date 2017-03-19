36ac5492def097c22da5250d6f39e4e998d70058
Lewis Wright <LewisW@users.noreply.github.com>
2017-03-19 06:12:11 +0000

    Bullets for your brain today

--- src/Actions/OwnCloudFilesChangedAction.php
+++ src/Actions/OwnCloudFilesChangedAction.php
@@ -8,34 +8,74 @@ use Slim\Http\Response;
 
 class OwnCloudFilesChangedAction extends AbstractAction
 {
+    private static $lyrics = [
+        "The future teaches you to be alone",
+        "The present to be afraid and cold",
+        "So if I can shoot rabbits",
+        "Then I can shoot fascists",
+        "Bullets for your brain today",
+        "But we'll forget it all again",
+        "Monuments put from pen to paper",
+        "Turns me into a gutless wonder",
+        "And if you tolerate this",
+        "Then your children will be next",
+        "And if you tolerate this",
+        "Then your children will be next",
+        "Will be next",
+        "Will be next",
+        "Will be next",
+        "Gravity keeps my head down",
+        "Or is it maybe shame",
+        "At being so young and being so vain",
+        "Holes in your head today",
+        "But I'm a pacifist",
+        "I've walked La Ramblas",
+        "But not with real intent",
+        "And if you tolerate this",
+        "Then your children will be next",
+        "And if you tolerate this",
+        "Then your children will be next",
+        "Will be next",
+        "Will be next",
+        "Will be next",
+        "Will be next",
+        "And on the street tonight an old man plays",
+        "With newspaper cuttings of his glory days",
+        "And if you tolerate this",
+        "Then your children will be next",
+        "And if you tolerate this",
+        "Then your children will be next",
+        "Will be next",
+        "Will be next",
+        "Will be next"
+    ];
+
     function __invoke(Request $request, Response $response, array $args)
     {
-        // Extract JSON
-//        $owncloudPath = '/owncloud/';
-//        $fullpath = 'my/project';
-//
-//        list($user, $path) = explode('/', $fullpath, 2);
+        /**
+         * Contains keys: username
+         */
+        $body = $request->getParsedBody();
 
         $git = new Git();
 
         // Set author
         $git->config([
             'global' => [
-                'user.name' => "Chris Emerson"
+                'user.name' => $body['username']
             ]
         ]);
 
         $git->setRepository("c:\\code\\hack24-2017-documents");
 
-        $branchName = 'chrisemerson';
-
         try {
-            $git->checkout($branchName);
+            $git->checkout($body['username']);
         } catch (GitException $e) {
-            $git->checkout->create($branchName);
+            $git->checkout->create($body['username']);
         }
 
         // Commit
+        $git->commit(self::$lyrics[array_rand(self::$lyrics)]);
 
         // Merge develop into user's branch, check that no conflicts occur
         try {
@@ -45,6 +85,6 @@ class OwnCloudFilesChangedAction extends AbstractAction
             $git->merge->abort();
         }
 
-        $git->push('origin', $branchName);
+        $git->push('origin', $body['username']);
     }
 }
