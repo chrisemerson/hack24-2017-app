d19835c97c60eccaa3840cf4211453fe180cb9b6
Chris Emerson <chris@cemerson.co.uk>
2017-03-19 02:21:01 +0000

    Add notes on code to be put into action

--- src/Actions/OwnCloudFilesChangedAction.php
+++ src/Actions/OwnCloudFilesChangedAction.php
@@ -8,6 +8,14 @@ class OwnCloudFilesChangedAction extends AbstractAction
 {
     function __invoke(Request $request, Response $response, array $args)
     {
+        // Extract JSON
 
+        // Commit files to git, using separate branches per user
+
+        // Merge develop into user's branch, check that no conflicts occur
+
+        // If no conflicts, merge user's branch into develop
+
+        // Merge develop into branches of all other users to 'share' content perhaps?
     }
 }
